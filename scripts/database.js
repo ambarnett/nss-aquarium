40d4980a64a904843606d1c5f805798a60415104
ambarnett <andy.barnett09@gmail.com>
2021-05-27 11:45:01 -0500

    first add of js

---
+++ scripts/database.js
@@ -0,0 +1,52 @@
+/*
+* To get you started, here's some properties of Bart.
+* You need to add more properties to complete his 
+* representation as an object. Then add all the other
+* fish to the collection.
+*/
+const database = {
+    fish: [
+        {
+            name: "Bart",
+            food: "crustaceans",
+            size: 3,
+            from: "Florida Keys",
+            species: "Fish"
+        },
+        {
+            name: "Nemo",
+            food: "snails",
+            size: 2,
+            from: "Florida Keys",
+            species: "Fish"
+        },
+        {
+            name: "Red",
+            food: "shrimp",
+            size: 5,
+            from: "Florida Keys",
+            species: "Lobster"
+        },
+        {
+            name: "Fred",
+            food: "crustaceans",
+            size: 3,
+            from: "Florida Keys",
+            species: "Fish"
+        },
+        {
+            name: "Jack",
+            food: "worms",
+            size: 6,
+            from: "French Broad River",
+            species: "Large Mouth Bass"
+        },
+        {
+            name: "Tim",
+            food: "crustaceans",
+            size: 3,
+            from: "Florida Keys",
+            species: "Fish"
+        }
+    ]
+}
